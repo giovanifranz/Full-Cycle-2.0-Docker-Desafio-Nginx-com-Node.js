834b47d96d62c1db497a30e581d6f470f380461e
Giovani Franz <giovanifranz151@gmail.com>
2021-12-13 17:58:31 -0300

    correção banco de dados
    Eu já havia criado a tabela e acabei rodando a aplicação sem validar isso, agora está correta.

--- node/index.js
+++ node/index.js
@@ -1,36 +1,41 @@
-const express = require('express')
-const mysql = require('mysql2/promise')
+const express = require("express");
+const mysql = require("mysql2/promise");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 const config = {
-    database:'nodedb',
-    host: 'db',
-    password: 'root',
-    user: 'root'     
-}
+  database: "nodedb",
+  host: "db",
+  password: "root",
+  user: "root",
+};
 
-app.get('/', async (req, res) => {
-    const connection = await mysql.createConnection(config)
-    const sql = `INSERT INTO people (name) values ('Giovani')`
-    await connection.execute(sql)
-    const [rows] = await connection.query('select * from people')
-        
-    let data = '<h1>Full Cycle Rocks!</h1>';
-    data = data + '<br>Nomes Cadastrados no MySQL:<ul>';
+app.get("/", async (req, res) => {
+  const connection = await mysql.createConnection(config);
+  await connection.connect();
 
-    for (const row of rows){
-        data = data + '<li>' + row.name + '</li>'
-    }
-    
-    data = data + '</ul>';
- 
-    await connection.end()
+  await connection.execute(
+    "CREATE TABLE IF NOT EXISTS people (name VARCHAR(255))"
+  );
+  const sql = `INSERT INTO people (name) values ('Giovani')`;
+  await connection.execute(sql);
+  const [rows] = await connection.query("select * from people");
 
-    res.status(200).send(data)    
-})
+  let data = "<h1>Full Cycle Rocks!</h1>";
+  data = data + "<br>Nomes Cadastrados no MySQL:<ul>";
 
-app.listen(port, async() => {    
-    console.log(`Rodando na porta  ${port}`)
-})
+  for (const row of rows) {
+    data = data + "<li>" + row.name + "</li>";
+  }
+
+  data = data + "</ul>";
+
+  await connection.end();
+
+  res.status(200).send(data);
+});
+
+app.listen(port, async () => {
+  console.log(`Rodando na porta  ${port}`);
+});
