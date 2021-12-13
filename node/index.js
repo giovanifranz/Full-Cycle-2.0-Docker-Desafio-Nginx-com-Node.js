1a8a61144d4f99ab9e5cae35fe73174caee7e090
giovanifranz <giovanifranz151@gmail.com>
2021-12-13 16:54:54 -0300

    first commit

---
+++ node/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mysql = require('mysql2/promise')
+
+const app = express()
+const port = 3000
+
+const config = {
+    database:'nodedb',
+    host: 'db',
+    password: 'root',
+    user: 'root'     
+}
+
+app.get('/', async (req, res) => {
+    const connection = await mysql.createConnection(config)
+    const sql = `INSERT INTO people (name) values ('Giovani')`
+    await connection.execute(sql)
+    const [rows] = await connection.query('select * from people')
+        
+    let data = '<h1>Full Cycle Rocks!</h1>';
+    data = data + '<br>Nomes Cadastrados no MySQL:<ul>';
+
+    for (const row of rows){
+        data = data + '<li>' + row.name + '</li>'
+    }
+    
+    data = data + '</ul>';
+ 
+    await connection.end()
+
+    res.status(200).send(data)    
+})
+
+app.listen(port, async() => {    
+    console.log(`Rodando na porta  ${port}`)
+})
